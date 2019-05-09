65c5bd43838a2fc4b149f651da22bb44a2934b29
SGonzalez96 <47160020+SGonzalez96@users.noreply.github.com>
2019-05-09 12:59:39 -0700

    updated sketch

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 function preload() {
   img1 = loadImage ("makeup35comments.jpg");
   img2 = loadImage ("nomakeup35comments.jpg");
-  img3 = loadImage ("makeup35glitchcomments2");
-  img4 = loadImage ("nomakeup35glitchcomments");
+  img3 = loadImage ("makeup35glitchcomments2.jpg");
+  img4 = loadImage ("nomakeup35glitchcomments.jpg");
 }
 
 function setup() {
@@ -13,13 +13,13 @@ function setup() {
 function draw() {
   background(204);
   if ((mouseX <= 300) && (mouseY <= 500)) {
-    image(img2,0, 0, 300, 500);   // Upper-left
+    image(img2, 0, 0, 300, 500);   // Upper-left
   }
   else if ((mouseX <= 300) && (mouseY > 500)) {
     image(img3, 0, 500, 300, 500);  // Lower-left
   }
   else if ((mouseX > 300) && (mouseY <= 500)) {
-    rect(img4, 300, 0, 500, 500);  // Upper-right
+    image(img4, 300, 0, 300, 500);  // Upper-right
   }
   else {
     image(img1, 300, 500, 300, 500); // Lower-right
